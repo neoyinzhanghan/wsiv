70f529190694e274186827b54328d94b39457683
Neo Yin <yinzhang@berkeley.edu>
2024-10-28 16:29:29 -0700

    minimal viable implementation

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import ImageViewer from "../components/ImageViewer";
-import '../styles/globals.css';
 
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState(null);
